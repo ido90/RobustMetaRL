ad0b41fb4f8f7b60a36f9cd544a7322ed79b870f
ido90 <idogreenberg90@gmail.com>
2022-10-18 14:40:43 +0300

    initial gif-generator code (not working yet)

---
+++ run_agent.py
@@ -0,0 +1,213 @@
+import argparse
+import os
+import warnings
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+import imageio
+from environments.parallel_envs import make_vec_envs
+from utils import helpers as utl
+import analysis
+from config.mujoco import \
+    args_cheetah_vel_varibad, args_cheetah_mass_varibad, args_cheetah_body_varibad, \
+    args_humanoid_body_varibad
+from metalearner import MetaLearner
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+
+
+def run_agent(learner, tasks=None, num_episodes=None, gif='demo'):
+    # GIF args
+    dur = 0.01
+    width = 250
+    height = 200
+    gif += '.gif'
+
+    ret_rms = learner.envs.venv.ret_rms if learner.args.norm_rew_for_policy else None
+    args = learner.args
+    policy = learner.policy
+    encoder = learner.vae.encoder
+    iter_idx = learner.args.seed
+
+    env_name = args.env_name
+    if hasattr(args, 'test_env_name'):
+        env_name = args.test_env_name
+    if num_episodes is None:
+        num_episodes = args.max_rollouts_per_task
+    num_processes = args.num_processes
+
+    # --- set up the things we want to log ---
+
+    # for each process, we log the returns during the first, second, ... episode
+    # (such that we have a minimum of [num_episodes]; the last column is for
+    #  any overflow and will be discarded at the end, because we need to wait until
+    #  all processes have at least [num_episodes] many episodes)
+    returns_per_episode = torch.zeros((num_processes, num_episodes + 1)).to(device)
+
+    # --- initialise environments and latents ---
+
+    envs = make_vec_envs(env_name,
+                         seed=args.seed * 42 + iter_idx,
+                         num_processes=num_processes,
+                         gamma=args.policy_gamma,
+                         device=device,
+                         rank_offset=num_processes + 1,  # to use diff tmp folders than main processes
+                         episodes_per_task=num_episodes,
+                         normalise_rew=args.norm_rew_for_policy,
+                         ret_rms=ret_rms,
+                         tasks=tasks,
+                         add_done_info=args.max_rollouts_per_task > 1,
+                         eval_mode=True,
+                         )
+    num_steps = envs._max_episode_steps
+
+    # reset environments
+    state, belief, task = utl.reset_env(envs, args)
+    tasks = envs.get_task()
+    print('Tasks:')
+    print(tasks)
+
+    # this counts how often an agent has done the same task already
+    task_count = torch.zeros(num_processes).long().to(device)
+
+    if encoder is not None:
+        # reset latent state to prior
+        latent_sample, latent_mean, latent_logvar, hidden_state = encoder.prior(num_processes)
+    else:
+        latent_sample = latent_mean = latent_logvar = hidden_state = None
+
+    for episode_idx in range(num_episodes):
+
+        ep_obs = []
+
+        for step_idx in range(num_steps):
+
+            with torch.no_grad():
+                _, action = utl.select_action(args=args,
+                                              policy=policy,
+                                              state=state,
+                                              belief=belief,
+                                              task=task,
+                                              latent_sample=latent_sample,
+                                              latent_mean=latent_mean,
+                                              latent_logvar=latent_logvar,
+                                              deterministic=True)
+
+            # observe reward and next obs
+            [state, belief, task], (rew_raw, rew_normalised), done, infos = utl.env_step(envs, action, args)
+            done_mdp = [info['done_mdp'] for info in infos]
+
+            if encoder is not None:
+                # update the hidden state
+                latent_sample, latent_mean, latent_logvar, hidden_state = utl.update_encoding(encoder=encoder,
+                                                                                              next_obs=state,
+                                                                                              action=action,
+                                                                                              reward=rew_raw,
+                                                                                              done=None,
+                                                                                              hidden_state=hidden_state)
+
+            # add rewards
+            returns_per_episode[range(num_processes), task_count] += rew_raw.view(-1)
+
+            for i in np.argwhere(done_mdp).flatten():
+                # count task up, but cap at num_episodes + 1
+                task_count[i] = min(task_count[i] + 1, num_episodes)  # zero-indexed, so no +1
+            if np.sum(done) > 0:
+                done_indices = np.argwhere(done.flatten()).flatten()
+                state, belief, task = utl.reset_env(envs, args, indices=done_indices, state=state)
+
+            # update GIF
+            if gif:
+                env = envs.venv.unwrapped.envs[0].unwrapped
+                # TODO https://github.com/openai/mujoco-py/issues/187
+                obs = env.render(mode='rgb_array') #, width=width, height=height)
+                print(obs.shape)
+                # assert obs.shape == (height, width, 3), obs.shape
+                ep_obs.append(obs)
+
+        # save GIF
+        if gif:
+            fname = gif
+            if num_episodes > 1:
+                fname += f'_{episode_idx}'
+            fname += '.gif'
+            with imageio.get_writer(fname, mode='I', duration=dur) as writer:
+                for obs_np in ep_obs:
+                    writer.append_data(obs_np)
+
+    returns_per_episode = returns_per_episode[:, :num_episodes]
+    print('Returns:')
+    print(returns_per_episode)
+
+    envs.close()
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--env-type', default='cheetah_vel_varibad')
+    args, rest_args = parser.parse_known_args()
+    env = args.env_type
+
+    if env == 'cheetah_vel_varibad':
+        args = args_cheetah_vel_varibad.get_args(rest_args)
+    elif env == 'cheetah_mass_varibad':
+        args = args_cheetah_mass_varibad.get_args(rest_args)
+    elif env == 'cheetah_body_varibad':
+        args = args_cheetah_body_varibad.get_args(rest_args)
+    elif env == 'humanoid_body_varibad':
+        args = args_humanoid_body_varibad.get_args(rest_args)
+    else:
+        raise Exception("Invalid Environment")
+
+    short_name = dict(cheetah_vel_varibad='hcv', cheetah_mass_varibad='hcm',
+                      cheetah_body_varibad='hcb', humanoid_body_varibad='humb')[env]
+    method = 'varibad'
+    if args.cem:
+        if args.oracle:
+            method = 'oracbad'
+        else:
+            method = 'cembad'
+    elif args.tail == 1:
+        method = 'cvarbad'
+    elif args.tail == 2:
+        method = 'schedbad'
+
+    # set args for demo run
+    args.deterministic_execution = True
+    args.num_processes = 1
+    args.results_log_dir = 'tmp_logs'
+
+    # warning for deterministic execution
+    if args.deterministic_execution:
+        print('Envoking deterministic code execution.')
+        if torch.backends.cudnn.enabled:
+            warnings.warn('Running with deterministic CUDNN.')
+
+    # clean up arguments
+    if args.disable_metalearner or args.disable_decoder:
+        args.decode_reward = False
+        args.decode_state = False
+        args.decode_task = False
+
+    if hasattr(args, 'decode_only_past') and args.decode_only_past:
+        args.split_batches_by_elbo = True
+
+    # begin training (loop through all passed seeds)
+    seed_list = [args.seed] if isinstance(args.seed, int) else args.seed
+    for seed in seed_list:
+        print('running', seed)
+        args.seed = seed
+        args.action_space = None
+        base_path = analysis.get_base_path(args.env_name)
+        dir = analysis.get_dir(base_path, short_name, method, args.seed)
+        pth = f'{base_path}/{dir}/best_models'
+        learner = MetaLearner(args)
+        learner.load_model(save_path=pth)
+        run_agent(learner, gif=f'{short_name}_{method}_{args.seed}')
+        # learner.test(load='final')
+        # learner.test(load='best')
+
+
+if __name__ == '__main__':
+    main()
