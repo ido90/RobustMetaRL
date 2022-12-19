4752f609449545db4355006f30354525b5700cc8
ido90 <idogreenberg90@gmail.com>
2022-12-19 19:53:17 +0200

    unused configs cleanup

--- main.py
+++ main.py
@@ -13,21 +13,9 @@ import wandb
 
 # get configs
 from config import args_khazad_dum_varibad
-from config.gridworld import \
-    args_grid_belief_oracle, args_grid_rl2, args_grid_varibad
-from config.pointrobot import \
-    args_pointrobot_multitask, args_pointrobot_varibad, args_pointrobot_rl2, args_pointrobot_humplik
-from config.mujoco import \
-    args_cheetah_dir_multitask, args_cheetah_dir_expert, args_cheetah_dir_rl2, args_cheetah_dir_varibad, \
-    args_cheetah_vel_multitask, args_cheetah_vel_expert, args_cheetah_vel_rl2, args_cheetah_vel_varibad, \
-    args_cheetah_vel_avg, \
-    args_cheetah_mass_varibad, args_cheetah_body_varibad, \
-    args_ant_dir_multitask, args_ant_dir_expert, args_ant_dir_rl2, args_ant_dir_varibad, \
-    args_ant_goal_multitask, args_ant_goal_expert, args_ant_goal_rl2, args_ant_goal_varibad, \
-    args_ant_goal_humplik, \
-    args_walker_multitask, args_walker_expert, args_walker_avg, args_walker_rl2, args_walker_varibad, \
-    args_humanoid_dir_varibad, args_humanoid_dir_rl2, args_humanoid_dir_multitask, args_humanoid_dir_expert, \
-    args_humanoid_mass_varibad, args_humanoid_body_varibad
+from config.mujoco import args_cheetah_vel_rl2, args_cheetah_vel_varibad, args_cheetah_mass_varibad, \
+    args_cheetah_body_varibad,  args_ant_goal_rl2, args_ant_goal_varibad, args_humanoid_mass_varibad, \
+    args_humanoid_body_varibad
 from environments.parallel_envs import make_vec_envs
 from learner import Learner
 from metalearner import MetaLearner
@@ -71,48 +59,12 @@ def main():
     args, rest_args = parser.parse_known_args()
     env = args.env_type
 
-    # --- GridWorld ---
-
-    if env == 'gridworld_belief_oracle':
-        args = args_grid_belief_oracle.get_args(rest_args)
-    elif env == 'gridworld_varibad':
-        args = args_grid_varibad.get_args(rest_args)
-    elif env == 'gridworld_rl2':
-        args = args_grid_rl2.get_args(rest_args)
-
-    # --- PointRobot 2D Navigation ---
-
-    elif env == 'pointrobot_multitask':
-        args = args_pointrobot_multitask.get_args(rest_args)
-    elif env == 'pointrobot_varibad':
-        args = args_pointrobot_varibad.get_args(rest_args)
-    elif env == 'pointrobot_rl2':
-        args = args_pointrobot_rl2.get_args(rest_args)
-    elif env == 'pointrobot_humplik':
-        args = args_pointrobot_humplik.get_args(rest_args)
-
-    elif env == 'khazad_dum_varibad':
+    if env == 'khazad_dum_varibad':
         args = args_khazad_dum_varibad.get_args(rest_args)
 
     # --- MUJOCO ---
 
-    # - CheetahDir -
-    elif env == 'cheetah_dir_multitask':
-        args = args_cheetah_dir_multitask.get_args(rest_args)
-    elif env == 'cheetah_dir_expert':
-        args = args_cheetah_dir_expert.get_args(rest_args)
-    elif env == 'cheetah_dir_varibad':
-        args = args_cheetah_dir_varibad.get_args(rest_args)
-    elif env == 'cheetah_dir_rl2':
-        args = args_cheetah_dir_rl2.get_args(rest_args)
-    #
     # - CheetahVel -
-    elif env == 'cheetah_vel_multitask':
-        args = args_cheetah_vel_multitask.get_args(rest_args)
-    elif env == 'cheetah_vel_expert':
-        args = args_cheetah_vel_expert.get_args(rest_args)
-    elif env == 'cheetah_vel_avg':
-        args = args_cheetah_vel_avg.get_args(rest_args)
     elif env == 'cheetah_vel_varibad':
         args = args_cheetah_vel_varibad.get_args(rest_args)
     elif env == 'cheetah_vel_rl2':
@@ -124,49 +76,11 @@ def main():
     elif env == 'cheetah_body_varibad':
         args = args_cheetah_body_varibad.get_args(rest_args)
     #
-    # - AntDir -
-    elif env == 'ant_dir_multitask':
-        args = args_ant_dir_multitask.get_args(rest_args)
-    elif env == 'ant_dir_expert':
-        args = args_ant_dir_expert.get_args(rest_args)
-    elif env == 'ant_dir_varibad':
-        args = args_ant_dir_varibad.get_args(rest_args)
-    elif env == 'ant_dir_rl2':
-        args = args_ant_dir_rl2.get_args(rest_args)
-    #
     # - AntGoal -
-    elif env == 'ant_goal_multitask':
-        args = args_ant_goal_multitask.get_args(rest_args)
-    elif env == 'ant_goal_expert':
-        args = args_ant_goal_expert.get_args(rest_args)
     elif env == 'ant_goal_varibad':
         args = args_ant_goal_varibad.get_args(rest_args)
-    elif env == 'ant_goal_humplik':
-        args = args_ant_goal_humplik.get_args(rest_args)
     elif env == 'ant_goal_rl2':
         args = args_ant_goal_rl2.get_args(rest_args)
-    #
-    # - Walker -
-    elif env == 'walker_multitask':
-        args = args_walker_multitask.get_args(rest_args)
-    elif env == 'walker_expert':
-        args = args_walker_expert.get_args(rest_args)
-    elif env == 'walker_avg':
-        args = args_walker_avg.get_args(rest_args)
-    elif env == 'walker_varibad':
-        args = args_walker_varibad.get_args(rest_args)
-    elif env == 'walker_rl2':
-        args = args_walker_rl2.get_args(rest_args)
-    #
-    # - HumanoidDir -
-    elif env == 'humanoid_dir_multitask':
-        args = args_humanoid_dir_multitask.get_args(rest_args)
-    elif env == 'humanoid_dir_expert':
-        args = args_humanoid_dir_expert.get_args(rest_args)
-    elif env == 'humanoid_dir_varibad':
-        args = args_humanoid_dir_varibad.get_args(rest_args)
-    elif env == 'humanoid_dir_rl2':
-        args = args_humanoid_dir_rl2.get_args(rest_args)
         #
         # - HumanoidBody -
     elif env == 'humanoid_mass_varibad':
