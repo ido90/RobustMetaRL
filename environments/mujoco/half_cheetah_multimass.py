45584d79487cce90402b99d555e8bd41b7f174e0
Eli Meirom <eli.meirom@gmail.com>
2023-03-17 13:34:48 +0200

    Half Cheetah Multi-Mass sweep

--- environments/mujoco/half_cheetah_multimass.py
+++ environments/mujoco/half_cheetah_multimass.py
@@ -1,89 +1,198 @@
-import random
-
-import numpy as np
-
-from gym.spaces import Box
-from .half_cheetah import HalfCheetahEnv
+import argparse
+from utils.helpers import boolean_argument
 
 
-class HalfCheetahMassEnv(HalfCheetahEnv):
-    """Half-cheetah environment with varying Mass. The code is adapted from
-    https://github.com/lmzintgraf/varibad/blob/master/environments/mujoco/half_cheetah_vel.py
+def get_args(rest_args):
+    parser = argparse.ArgumentParser()
 
-    The half-cheetah follows the dynamics and rewards from MuJoCo.
-    Its tasks correspond to different values of cheetah body mass,
-    sampled log-uniformly in the range of [50%, 200%] of their original values.
-    """
+    # --- GENERAL ---
 
-    def __init__(self, max_episode_steps=200, eval_mode=False):
-        self.eval_mode = eval_mode
-        self._max_episode_steps = max_episode_steps
-        self.task_dim = 8
-        super(HalfCheetahMassEnv, self).__init__()
-        self.observation_space = Box(low=-np.inf, high=np.inf, shape=(20,),
-                                     dtype=np.float64)
+    parser.add_argument('--use_wandb', type=bool, default=False, help='use wandb')
+    parser.add_argument('--num_frames', type=int, default=8e7, help='number of frames to train')
+    parser.add_argument('--valid_tasks', type=int, default=32, help='number of validation tasks')
+    parser.add_argument('--test_tasks', type=int, default=1000, help='number of test tasks')
+    parser.add_argument('--max_rollouts_per_task', type=int, default=2, help='number of MDP episodes for adaptation')
+    parser.add_argument('--exp_label', default='', help='label (typically name of method)')
+    parser.add_argument('--exp_suffix', default='', help='additional label suffix')
+    parser.add_argument('--env_name', default='HalfCheetahMultiMass-v0', help='environment to train on')
 
-        self.original_mass_vec = self.model.body_mass.copy()  # 8 elements
-        self.set_task(self.sample_task())
+    parser.add_argument('--alpha', type=float, default=0.05, help='CVaR risk level')
+    parser.add_argument('--tail', type=int, default=0, help='learn from all batch / tail / scheduled-tail')
+    parser.add_argument('--cem', type=int, default=0, help='use CEM sampler (none / strict / scheduled)')
+    parser.add_argument('--oracle', type=bool, default=False, help='use oracle CEM sampler')
 
-        self._time = 0
-        self._return = 0
-        self._last_return = 0
-        self._curr_rets = []
+    # --- POLICY ---
 
-    def step(self, action):
-        xposbefore = self.sim.data.qpos[0]
-        self.do_simulation(action, self.frame_skip)
-        xposafter = self.sim.data.qpos[0]
+    # what to pass to the policy (note this is after the encoder)
+    parser.add_argument('--pass_state_to_policy', type=boolean_argument, default=True, help='condition policy on state')
+    parser.add_argument('--pass_latent_to_policy', type=boolean_argument, default=True, help='condition policy on VAE latent')
+    parser.add_argument('--pass_belief_to_policy', type=boolean_argument, default=False, help='condition policy on ground-truth belief')
+    parser.add_argument('--pass_task_to_policy', type=boolean_argument, default=False, help='condition policy on ground-truth task description')
 
-        forward_reward = (xposafter - xposbefore) / self.dt
-        ctrl_cost = 0.5 * 1e-1 * np.sum(np.square(action))
+    # using separate encoders for the different inputs ("None" uses no encoder)
+    parser.add_argument('--policy_state_embedding_dim', type=int, default=64)
+    parser.add_argument('--policy_latent_embedding_dim', type=int, default=64)
+    parser.add_argument('--policy_belief_embedding_dim', type=int, default=None)
+    parser.add_argument('--policy_task_embedding_dim', type=int, default=None)
 
-        observation = self._get_obs()
-        # this value is in [-0.1,0.1] when the cheetah is straight, and in [-0.6,-0.4] when it's upside-down.
-        #  we penalize the cheetah being upside down.
-        reward_height = observation[0]
-        reward = forward_reward - ctrl_cost + reward_height
-        done = False
-        infos = dict(reward_forward=forward_reward,
-                     reward_ctrl=-ctrl_cost,
-                     task=self.get_task())
-        self._time += 1
-        self._return += reward
-        if self._time % self._max_episode_steps == 0:
-            # print(f'[{self._time//self._max_episode_steps}] '
-            #       f'{self.task},\t{self._return}')
-            self._last_return = self._return
-            self._curr_rets.append(self._return)
-            self._return = 0
-        return observation, reward, done, infos
+    # normalising (inputs/rewards/outputs)
+    parser.add_argument('--norm_state_for_policy', type=boolean_argument, default=True, help='normalise state input')
+    parser.add_argument('--norm_latent_for_policy', type=boolean_argument, default=True, help='normalise latent input')
+    parser.add_argument('--norm_belief_for_policy', type=boolean_argument, default=True, help='normalise belief input')
+    parser.add_argument('--norm_task_for_policy', type=boolean_argument, default=True, help='normalise task input')
+    parser.add_argument('--norm_rew_for_policy', type=boolean_argument, default=True, help='normalise rew for RL train')
+    parser.add_argument('--norm_actions_pre_sampling', type=boolean_argument, default=False, help='normalise policy output')
+    parser.add_argument('--norm_actions_post_sampling', type=boolean_argument, default=False, help='normalise policy output')
 
-    def get_last_return(self):
-        return np.sum(self._curr_rets)
+    # network
+    parser.add_argument('--policy_layers', nargs='+', default=[128, 128])
+    parser.add_argument('--policy_activation_function', type=str, default='tanh', help='tanh/relu/leaky-relu')
+    parser.add_argument('--policy_initialisation', type=str, default='normc', help='normc/orthogonal')
+    parser.add_argument('--policy_anneal_lr', type=boolean_argument, default=False, help='anneal LR over time')
 
-    def set_task(self, task):
-        self.task = task
-        for i in range(len(self.model.body_mass)):
-            self.model.body_mass[i] = task[i] * self.original_mass_vec[i]
-        self.model.geom_size[1:, 0] = 0.046 * task[i]
-        return task
+    # RL algorithm
+    parser.add_argument('--policy', type=str, default='ppo', help='choose: a2c, ppo')
+    parser.add_argument('--policy_optimiser', type=str, default='adam', help='choose: rmsprop, adam')
 
-    def get_task(self):
-        return self.task
+    # PPO specific
+    parser.add_argument('--ppo_num_epochs', type=int, default=2, help='number of epochs per PPO update')
+    parser.add_argument('--ppo_num_minibatch', type=int, default=8, help='number of minibatches to split the data')
+    parser.add_argument('--ppo_use_huberloss', type=boolean_argument, default=True, help='use huberloss instead of MSE')
+    parser.add_argument('--ppo_use_clipped_value_loss', type=boolean_argument, default=True, help='clip value loss')
+    parser.add_argument('--ppo_clip_param', type=float, default=0.1, help='clamp param')
 
-    def sample_task(self):
-        return np.array([2 ** random.uniform(-.5, .5)
-                         for _ in range(self.task_dim)])
+    # other hyperparameters
+    parser.add_argument('--lr_policy', type=float, default=7e-4, help='learning rate (default: 7e-4)')
+    parser.add_argument('--num_processes', type=int, default=16,
+                        help='how many training CPU processes / parallel environments to use (default: 16)')
+    parser.add_argument('--policy_num_steps', type=int, default=400,
+                        help='number of env steps to do (per process) before updating')
+    parser.add_argument('--policy_eps', type=float, default=1e-8, help='optimizer epsilon (1e-8 for ppo, 1e-5 for a2c)')
+    parser.add_argument('--policy_init_std', type=float, default=1.0, help='only used for continuous actions')
+    parser.add_argument('--policy_value_loss_coef', type=float, default=0.5, help='value loss coefficient')
+    parser.add_argument('--policy_entropy_coef', type=float, default=0.01, help='entropy term coefficient')
+    parser.add_argument('--policy_gamma', type=float, default=0.97, help='discount factor for rewards')
+    parser.add_argument('--policy_use_gae', type=boolean_argument, default=True,
+                        help='use generalized advantage estimation')
+    parser.add_argument('--policy_tau', type=float, default=0.9, help='gae parameter')
+    parser.add_argument('--use_proper_time_limits', type=boolean_argument, default=True,
+                        help='treat timeout and death differently (important in mujoco)')
+    parser.add_argument('--policy_max_grad_norm', type=float, default=0.5, help='max norm of gradients')
+    parser.add_argument('--encoder_max_grad_norm', type=float, default=1.0, help='max norm of gradients')
+    parser.add_argument('--decoder_max_grad_norm', type=float, default=1.0, help='max norm of gradients')
 
-    def sample_tasks(self, n_tasks):
-        return [self.sample_task() for _ in range(n_tasks)]
+    # --- VAE TRAINING ---
 
-    def reset_task(self, task):
-        if task is None:
-            task = self.sample_task()
-        self.set_task(task)
-        self._time = 0
-        self._last_return = self._return
-        self._curr_rets = []
-        self._return = 0
-        # self.reset()
+    # general
+    parser.add_argument('--lr_vae', type=float, default=0.001)
+    parser.add_argument('--size_vae_buffer', type=int, default=10000,
+                        help='how many trajectories (!) to keep in VAE buffer')
+    parser.add_argument('--precollect_len', type=int, default=5000,
+                        help='how many frames to pre-collect before training begins (useful to fill VAE buffer)')
+    parser.add_argument('--vae_buffer_add_thresh', type=float, default=1,
+                        help='probability of adding a new trajectory to buffer')
+    parser.add_argument('--vae_batch_num_trajs', type=int, default=10,
+                        help='how many trajectories to use for VAE update')
+    parser.add_argument('--tbptt_stepsize', type=int, default=50,
+                        help='stepsize for truncated backpropagation through time; None uses max (horizon of BAMDP)')
+    parser.add_argument('--vae_subsample_elbos', type=int, default=50,
+                        help='for how many timesteps to compute the ELBO; None uses all')
+    parser.add_argument('--vae_subsample_decodes', type=int, default=50,
+                        help='number of reconstruction terms to subsample; None uses all')
+    parser.add_argument('--vae_avg_elbo_terms', type=boolean_argument, default=False,
+                        help='Average ELBO terms (instead of sum)')
+    parser.add_argument('--vae_avg_reconstruction_terms', type=boolean_argument, default=False,
+                        help='Average reconstruction terms (instead of sum)')
+    parser.add_argument('--num_vae_updates', type=int, default=1,
+                        help='how many VAE update steps to take per meta-iteration')
+    parser.add_argument('--pretrain_len', type=int, default=0, help='for how many updates to pre-train the VAE')
+    parser.add_argument('--kl_weight', type=float, default=0.1, help='weight for the KL term')
+
+    parser.add_argument('--split_batches_by_task', type=boolean_argument, default=False,
+                        help='split batches up by task (to save memory or if tasks are of different length)')
+    parser.add_argument('--split_batches_by_elbo', type=boolean_argument, default=False,
+                        help='split batches up by elbo term (to save memory of if ELBOs are of different length)')
+
+    # - encoder
+    parser.add_argument('--action_embedding_size', type=int, default=16)
+    parser.add_argument('--state_embedding_size', type=int, default=32)
+    parser.add_argument('--reward_embedding_size', type=int, default=16)
+    parser.add_argument('--encoder_layers_before_gru', nargs='+', type=int, default=[])
+    parser.add_argument('--encoder_gru_hidden_size', type=int, default=128, help='dimensionality of RNN hidden state')
+    parser.add_argument('--encoder_layers_after_gru', nargs='+', type=int, default=[])
+    parser.add_argument('--latent_dim', type=int, default=5, help='dimensionality of latent space')
+
+    # - decoder: rewards
+    parser.add_argument('--decode_reward', type=boolean_argument, default=True, help='use reward decoder')
+    parser.add_argument('--rew_loss_coeff', type=float, default=1.0, help='weight for state loss (vs reward loss)')
+    parser.add_argument('--input_prev_state', type=boolean_argument, default=True, help='use prev state for rew pred')
+    parser.add_argument('--input_action', type=boolean_argument, default=True, help='use prev action for rew pred')
+    parser.add_argument('--reward_decoder_layers', nargs='+', type=int, default=[64, 32])
+    parser.add_argument('--multihead_for_reward', type=boolean_argument, default=False,
+                        help='one head per reward pred (i.e. per state)')
+    parser.add_argument('--rew_pred_type', type=str, default='deterministic',
+                        help='choose: '
+                             'bernoulli (predict p(r=1|s))'
+                             'categorical (predict p(r=1|s) but use softmax instead of sigmoid)'
+                             'deterministic (treat as regression problem)')
+
+    # - decoder: state transitions
+    parser.add_argument('--decode_state', type=boolean_argument, default=False, help='use state decoder')
+    parser.add_argument('--state_loss_coeff', type=float, default=1.0, help='weight for state loss')
+    parser.add_argument('--state_decoder_layers', nargs='+', type=int, default=[64, 32])
+    parser.add_argument('--state_pred_type', type=str, default='deterministic', help='choose: deterministic, gaussian')
+
+    # - decoder: ground-truth task ("varibad oracle", after Humplik et al. 2019)
+    parser.add_argument('--decode_task', type=boolean_argument, default=False, help='use task decoder')
+    parser.add_argument('--task_loss_coeff', type=float, default=1.0, help='weight for task loss')
+    parser.add_argument('--task_decoder_layers', nargs='+', type=int, default=[64, 32])
+    parser.add_argument('--task_pred_type', type=str, default='task_id', help='choose: task_id, task_description')
+
+    # --- ABLATIONS ---
+
+    # for the VAE
+    parser.add_argument('--disable_decoder', type=boolean_argument, default=False,
+                        help='train without decoder')
+    parser.add_argument('--disable_stochasticity_in_latent', type=boolean_argument, default=False,
+                        help='use auto-encoder (non-variational)')
+    parser.add_argument('--disable_kl_term', type=boolean_argument, default=False,
+                        help='dont use the KL regularising loss term')
+    parser.add_argument('--decode_only_past', type=boolean_argument, default=False,
+                        help='only decoder past observations, not the future')
+    parser.add_argument('--kl_to_gauss_prior', type=boolean_argument, default=False,
+                        help='KL term in ELBO to fixed Gaussian prior (instead of prev approx posterior)')
+
+    # combining vae and RL loss
+    parser.add_argument('--rlloss_through_encoder', type=boolean_argument, default=False,
+                        help='backprop rl loss through encoder')
+    parser.add_argument('--add_nonlinearity_to_latent', type=boolean_argument, default=False,
+                        help='Use relu before feeding latent to policy')
+    parser.add_argument('--vae_loss_coeff', type=float, default=1.0,
+                        help='weight for VAE loss (vs RL loss)')
+
+    # for the policy training
+    parser.add_argument('--sample_embeddings', type=boolean_argument, default=False,
+                        help='sample embedding for policy, instead of full belief')
+
+    # for other things
+    parser.add_argument('--disable_metalearner', type=boolean_argument, default=False,
+                        help='Train feedforward policy')
+    parser.add_argument('--single_task_mode', type=boolean_argument, default=False,
+                        help='train policy on one (randomly chosen) environment only')
+
+    # --- OTHERS ---
+
+    # logging, saving, evaluation
+    parser.add_argument('--log_interval', type=int, default=25, help='log interval, one log per n updates')
+    parser.add_argument('--save_interval', type=int, default=500, help='save interval, one save per n updates')
+    parser.add_argument('--save_intermediate_models', type=boolean_argument, default=False, help='save all models')
+    parser.add_argument('--eval_interval', type=int, default=25, help='eval interval, one eval per n updates')
+    parser.add_argument('--vis_interval', type=int, default=500, help='visualisation interval, one eval per n updates')
+    parser.add_argument('--results_log_dir', default=None, help='directory to save results (None uses ./logs)')
+
+    # general settings
+    parser.add_argument('--seed',  nargs='+', type=int, default=[73])
+    parser.add_argument('--deterministic_execution', type=boolean_argument, default=False,
+                        help='Make code fully deterministic. Expects 1 process and uses deterministic CUDNN')
+
+    return parser.parse_args(rest_args)
