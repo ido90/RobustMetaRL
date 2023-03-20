5ee23fdb27f36523fd670799fd0626b7f4e4dae1
Eli Meirom <eli.meirom@gmail.com>
2023-03-20 19:08:26 +0200

    HCMM with multivariate normal dist.

--- environments/mujoco/half_cheetah_multimass.py
+++ environments/mujoco/half_cheetah_multimass.py
@@ -19,6 +19,11 @@ class HalfCheetahMultiMassEnv(HalfCheetahEnv):
         self.eval_mode = eval_mode
         self._max_episode_steps = max_episode_steps
         self.task_dim = 8
+        self.task = None
+        self._time = 0
+        self._return = 0
+        self._last_return = 0
+        self._curr_rets = []
         super(HalfCheetahMultiMassEnv, self).__init__()
         self.observation_space = Box(low=-np.inf, high=np.inf, shape=(20,),
                                      dtype=np.float64)
@@ -26,10 +31,7 @@ class HalfCheetahMultiMassEnv(HalfCheetahEnv):
         self.original_mass_vec = self.model.body_mass.copy()  # 8 elements
         self.set_task(self.sample_task())
 
-        self._time = 0
-        self._return = 0
-        self._last_return = 0
-        self._curr_rets = []
+
 
     def step(self, action):
         xposbefore = self.sim.data.qpos[0]
